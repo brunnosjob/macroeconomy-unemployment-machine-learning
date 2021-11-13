519bb236e517499b68519e94e81423722cf26853
Bruno Rodrigues <74928057+brunnosjob@users.noreply.github.com>
2021-11-13 08:48:44 -0300

    Update economy_web_app.py

--- economy_web_app.py
+++ economy_web_app.py
@@ -7,8 +7,8 @@
 #Importando as bibliotecas
 import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 import streamlit as st
 import pickle
 
