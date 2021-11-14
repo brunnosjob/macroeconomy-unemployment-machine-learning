525f0dec46634b6921ee2b17c1207c0ffd09b2df
Bruno Rodrigues <74928057+brunnosjob@users.noreply.github.com>
2021-11-14 12:28:24 -0300

    Update economy_web_app.py

--- economy_web_app.py
+++ economy_web_app.py
@@ -124,5 +124,3 @@ if pag == 'Sobre o modelo':
     
     st.markdown('#### Modelo para estimativa da saúde fiscal a partir do percentual do PIB comprometido para com a dívida pública')
     st.markdown(' ')
-    
-    st.markdown('''
